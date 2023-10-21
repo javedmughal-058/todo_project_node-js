fd9addf175ceaf90d6b7fed924dee5ab9dedb578
javedmughal <javedmughal609@gmail.com>
2023-10-21 16:02:26 +0500

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,9 +4,20 @@ exports.register = async(req, res, next)=> {
 
     try{
         const {email, password} = req.body;
-        const successRes = await UserService.registerUser(email,password);
+        const user = await UserService.checkUser(email);
+        if(user == null){
+            const successRes = await UserService.registerUser(email,password);
 
-        res.json({status: true, msg: "User Registered Successfully"});
+            res.json({status: true, msg: "User Registered Successfully"});
+            
+        }
+        else{
+            res.status(200).json({
+                status : false,
+                msg : "Email Already Exist.!",
+            });
+        }
+        
 
     }catch(err){
         throw err;
@@ -21,30 +32,36 @@ exports.login = async(req, res, next)=> {
     try{
         const {email, password} = req.body;
         const user = await UserService.checkUser(email);
-        console.log("----------------- ",user);
-        if(!user){
+        // console.log("-------User---------- ",user);
+        if(user == null){
             res.status(200).json({
                 status : false,
                 msg : "User doesn't exist",
             });
         }
 
-        const isMatch = await user.commparePassword(password);
-        if(isMatch === false){
-            res.status(200).json({
-                status : false,
-                msg : "Password Invalid",
-            });
+        else{
+            const isMatch = await user.commparePassword(password);
+            // console.log("--------Match--------- ",isMatch);
+            if(isMatch == false){
+                res.status(200).json({
+                    status : false,
+                    msg : "Password Invalid",
+                });
+            }
+            else{
+                let tokenData = {_id:user._id, email: user.email};
+                const token = await UserService.generateToekn(tokenData,"secretKey", "1h");
+                res.status(200).json({
+                    status : true,
+                    msg    : "Login Success",
+                    email  : user.email,
+                    token  : token
+                });
+            }
+
         }
-
-        let tokenData = {_id:user._id, email: user.email};
-        const token = await UserService.generateToekn(tokenData,"secretKey", "1h");
-        res.status(200).json({
-            status : true,
-            email : user.email,
-            token : token
-        });
-
+        
 
     }catch(err){
         throw err;
